e014cc09f49bd44f9d46b687898b978215963435
Artem Dragunov <dragunovartem99@gmail.com>
2023-07-04 13:25:13 +0300

    Write basic logic

--- rps.py
+++ rps.py
@@ -1,11 +1,39 @@
+import random
+
 def get_choices():
     player_choice = input("[Rock, paper, scissors] Enter a choice => ")
+
+    options = ["rock", "paper", "scissors"]
+
+    computer_choice = random.choice(options)
+
     choices = {
         "player": player_choice,
-        "computer": "paper"
+        "computer": computer_choice
     }
+
+    print(get_result(player_choice, computer_choice))
 
     return choices
 
-choices = get_choices()
-print(choices)
+def get_result(first_option, second_option):
+    info = f"You chose {first_option}, computer chose {second_option}. "
+
+    result_messages = {
+        "draw": "It's a draw! 😐",
+        "win": "You win! 😀",
+        "lose": "You lose. 😢"
+    }
+
+    if first_option == second_option:
+        result = result_messages["draw"]
+
+    elif first_option == "rock":
+        if second_option == "paper":
+            result = result_messages["lose"]
+        else:
+            result = result_messages["win"]
+
+    return info + result
+
+get_choices()
